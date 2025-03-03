62becfe2d82bf9e8caebe51e5d2c43516427be3d
linden <linden@qq.com>
2025-03-03 20:29:06 +0800

    .

--- dao/dao.go
+++ dao/dao.go
@@ -1,11 +1,7 @@
 package dao
 
 import (
-	"encoding/json"
-	"fmt"
 	"helptrade/global"
-	"strconv"
-	"time"
 
 	"github.com/adshao/go-binance/v2/futures"
 	"gorm.io/gorm"
@@ -40,99 +36,6 @@ func GetAllOrder() ([]Order, error) {
 	list := make([]Order, 0)
 	err := global.DB.Model(Order{}).Find(&list).Order("time asc").Error
 	return list, err
-}
-
-type TmpCombineOrder struct {
-	CurrentPostion  float64
-	CurrentCumQuote float64
-	OriginOrders    []Order
-	Order           CombineOrder
-}
-
-// 这个应该卸写在service给定时任务执行
-func CombineAccountOrder() []CombineOrder {
-
-	// 仓位状态
-	tmpCombineOrder := make(map[string]TmpCombineOrder, 0)
-
-	// 合并数据
-	combineOrderList := make([]CombineOrder, 0)
-
-	list, _ := GetAllOrder()
-
-	for _, v := range list {
-		endFlag := false
-		executedQtyFloat, _ := strconv.ParseFloat(v.ExecutedQty, 64)
-		cumQuoteFloat, _ := strconv.ParseFloat(v.CumQuote, 64)
-		avgPriceFloat, _ := strconv.ParseFloat(v.AvgPrice, 64)
-		if executedQtyFloat == 0 { // 无效订单，开了没执行的后面关了的
-			continue
-		}
-
-		if _, ok := tmpCombineOrder[v.Symbol]; !ok {
-			tmpCombineOrder[v.Symbol] = TmpCombineOrder{}
-		}
-		tmpOrder := tmpCombineOrder[v.Symbol]
-
-		// 手续费
-		commission := GetTotalCommissionByOrderId(v.OrderId)
-		tmpOrder.Order.Commission += commission
-
-		if tmpOrder.CurrentPostion == 0 { // 新开仓
-			tmpOrder.Order.StartTime = v.Time
-			tmpOrder.Order.PositionSide = v.PositionSide
-			tmpOrder.Order.Side = v.Side
-			tmpOrder.Order.Symbol = v.Symbol
-			tmpOrder.Order.OpenPrice = avgPriceFloat
-			tmpOrder.Order.FirstOpenCumQuote = cumQuoteFloat
-		} else if v.Side != tmpOrder.Order.Side && executedQtyFloat-tmpOrder.CurrentPostion == 0 { //结束
-			endFlag = true
-			tmpOrder.Order.EndTime = v.Time
-			tmpOrder.Order.ClosePrice = avgPriceFloat
-		}
-
-		if v.Side == tmpOrder.Order.Side {
-			tmpOrder.CurrentPostion += executedQtyFloat
-			tmpOrder.CurrentCumQuote += cumQuoteFloat
-			tmpOrder.Order.TotalOpenCumQuote += cumQuoteFloat
-		} else {
-			tmpOrder.CurrentPostion -= executedQtyFloat
-			tmpOrder.CurrentCumQuote -= cumQuoteFloat
-			tmpOrder.Order.TotalCloseCumQuote += cumQuoteFloat
-		}
-		if tmpOrder.CurrentCumQuote > tmpOrder.Order.MaxCumQuote {
-			tmpOrder.Order.MaxCumQuote = tmpOrder.CurrentCumQuote
-		}
-
-		tmpOrder.OriginOrders = append(tmpOrder.OriginOrders, v)
-
-		tmpCombineOrder[v.Symbol] = tmpOrder
-
-		if endFlag {
-			diff := tmpOrder.Order.TotalCloseCumQuote - tmpOrder.Order.TotalOpenCumQuote
-			tmpOrder.Order.PnL = diff
-			if tmpOrder.Order.Side == "BUY" {
-				tmpOrder.Order.PnL = diff
-			} else {
-				tmpOrder.Order.PnL = -diff
-			}
-			if tmpOrder.Order.MaxCumQuote < tmpOrder.Order.TotalCloseCumQuote {
-				tmpOrder.Order.MaxCumQuote = tmpOrder.Order.TotalCloseCumQuote
-			}
-
-			originOrdersStr, _ := json.Marshal(tmpOrder.OriginOrders)
-			tmpOrder.Order.OriginOrders = string(originOrdersStr)
-			combineOrderList = append(combineOrderList, tmpOrder.Order)
-			tmpCombineOrder[v.Symbol] = TmpCombineOrder{}
-		}
-	}
-
-	for _, v := range combineOrderList {
-		t := time.UnixMilli(v.StartTime).Format("2006-01-02 15:04:05")
-		fmt.Printf("%v %v %v pnl:%.2f \n", t, v.Side, v.Symbol, v.PnL)
-	}
-
-	return combineOrderList
 }
 
 func SaveCombineOrder(list []CombineOrder) error {
--- service/service.go
+++ service/service.go
@@ -2,9 +2,12 @@ package service
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"helptrade/dao"
 	"helptrade/global"
+	"strconv"
+	"time"
 
 	"github.com/adshao/go-binance/v2"
 )
@@ -14,7 +17,7 @@ func FetchAndCombineOrder() {
 
 	UpdateOrder()
 
-	list := dao.CombineAccountOrder()
+	list := CombineAccountOrder()
 	dao.UpsertCombineOrder(list)
 }
 
@@ -38,4 +41,96 @@ func UpdateOrder() {
 	for _, v := range res {
 		dao.UpsertOrder(v)
 	}
+}
+
+type TmpCombineOrder struct {
+	CurrentPostion  float64
+	CurrentCumQuote float64
+	OriginOrders    []dao.Order
+	Order           dao.CombineOrder
+}
+
+func CombineAccountOrder() []dao.CombineOrder {
+
+	// 仓位状态
+	tmpCombineOrder := make(map[string]TmpCombineOrder, 0)
+
+	// 合并数据
+	combineOrderList := make([]dao.CombineOrder, 0)
+
+	list, _ := dao.GetAllOrder()
+
+	for _, v := range list {
+		endFlag := false
+		executedQtyFloat, _ := strconv.ParseFloat(v.ExecutedQty, 64)
+		cumQuoteFloat, _ := strconv.ParseFloat(v.CumQuote, 64)
+		avgPriceFloat, _ := strconv.ParseFloat(v.AvgPrice, 64)
+		if executedQtyFloat == 0 { // 无效订单，开了没执行的后面关了的
+			continue
+		}
+
+		if _, ok := tmpCombineOrder[v.Symbol]; !ok {
+			tmpCombineOrder[v.Symbol] = TmpCombineOrder{}
+		}
+		tmpOrder := tmpCombineOrder[v.Symbol]
+
+		// 手续费
+		commission := dao.GetTotalCommissionByOrderId(v.OrderId)
+		tmpOrder.Order.Commission += commission
+
+		if tmpOrder.CurrentPostion == 0 { // 新开仓
+			tmpOrder.Order.StartTime = v.Time
+			tmpOrder.Order.PositionSide = v.PositionSide
+			tmpOrder.Order.Side = v.Side
+			tmpOrder.Order.Symbol = v.Symbol
+			tmpOrder.Order.OpenPrice = avgPriceFloat
+			tmpOrder.Order.FirstOpenCumQuote = cumQuoteFloat
+		} else if v.Side != tmpOrder.Order.Side && executedQtyFloat-tmpOrder.CurrentPostion == 0 { //结束
+			endFlag = true
+			tmpOrder.Order.EndTime = v.Time
+			tmpOrder.Order.ClosePrice = avgPriceFloat
+		}
+
+		if v.Side == tmpOrder.Order.Side {
+			tmpOrder.CurrentPostion += executedQtyFloat
+			tmpOrder.CurrentCumQuote += cumQuoteFloat
+			tmpOrder.Order.TotalOpenCumQuote += cumQuoteFloat
+		} else {
+			tmpOrder.CurrentPostion -= executedQtyFloat
+			tmpOrder.CurrentCumQuote -= cumQuoteFloat
+			tmpOrder.Order.TotalCloseCumQuote += cumQuoteFloat
+		}
+		if tmpOrder.CurrentCumQuote > tmpOrder.Order.MaxCumQuote {
+			tmpOrder.Order.MaxCumQuote = tmpOrder.CurrentCumQuote
+		}
+
+		tmpOrder.OriginOrders = append(tmpOrder.OriginOrders, v)
+
+		tmpCombineOrder[v.Symbol] = tmpOrder
+
+		if endFlag {
+			diff := tmpOrder.Order.TotalCloseCumQuote - tmpOrder.Order.TotalOpenCumQuote
+			tmpOrder.Order.PnL = diff
+			if tmpOrder.Order.Side == "BUY" {
+				tmpOrder.Order.PnL = diff
+			} else {
+				tmpOrder.Order.PnL = -diff
+			}
+			if tmpOrder.Order.MaxCumQuote < tmpOrder.Order.TotalCloseCumQuote {
+				tmpOrder.Order.MaxCumQuote = tmpOrder.Order.TotalCloseCumQuote
+			}
+
+			originOrdersStr, _ := json.Marshal(tmpOrder.OriginOrders)
+			tmpOrder.Order.OriginOrders = string(originOrdersStr)
+			combineOrderList = append(combineOrderList, tmpOrder.Order)
+			tmpCombineOrder[v.Symbol] = TmpCombineOrder{}
+		}
+	}
+
+	for _, v := range combineOrderList {
+		t := time.UnixMilli(v.StartTime).Format("2006-01-02 15:04:05")
+		fmt.Printf("%v %v %v pnl:%.2f \n", t, v.Side, v.Symbol, v.PnL)
+	}
+
+	return combineOrderList
 }
